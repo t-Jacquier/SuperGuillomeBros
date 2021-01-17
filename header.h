39df4e9f7e0619352186dafc7631360444fd0ea7
Timéo Jacquier <timeo.jacquier@hotmail.com>
2021-01-17 15:51:10 +0100

    création fenêtre

---
+++ header.h
@@ -0,0 +1,18 @@
+//
+// Created by timeojacquier on 17/01/2021.
+//
+
+#ifndef GUILLOME_HEADER_H
+#define GUILLOME_HEADER_H
+
+/**
+ * \brief Largeur en pixel de l'écran
+ */
+#define SCREEN_WIDTH 600
+
+/**
+ * \brief Hauteur en pixel de l'écran
+ */
+#define SCREEN_HEIGHT 600
+
+#endif //GUILLOME_HEADER_H
--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include "header.h"
 #include "librairies/fonction_SDL.h"
 
 int main() {
   SDL_Window* window;  // Déclaration de la fenêtre
   SDL_Event events;
+
+  if(SDL_Init(SDL_INIT_VIDEO) < 0) // Initialisation de la SDL
+  {
+    printf("Erreur d’initialisation de la SDL: %s",SDL_GetError());SDL_Quit();return EXIT_FAILURE;}// Créer la fenêtre
+  window = SDL_CreateWindow("Fenetre SDL", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE);
+  if(window == NULL) // En cas d’erreur
+  {
+    printf("Erreur de la creation d’une fenetre: %s",SDL_GetError());
+    SDL_Quit();
+    return EXIT_FAILURE;
+
+  }
+
+  SDL_Quit();
   return 0;
 }
